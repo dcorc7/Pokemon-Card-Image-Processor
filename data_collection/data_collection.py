6c9fb2e560358624c1ab60ae038bf8d94518c0f4
dcorc7 <dc1524@georgetown.edu>
2026-03-22 23:46:16 -0400

    Data collection

---
+++ data_collection/data_collection.py
@@ -0,0 +1,61 @@
+from pokemontcgsdk import Card, Set
+import json
+import os
+import requests
+
+# Function to collect Pokemon cards from pokemontcgsdk API
+def get_cards(output_path="./backend/app/index/metadata.json"):
+    all_cards = []
+    
+    # Option to collect every card
+    #cards = Card.all()
+
+    # Only original base1 cards
+    #cards = Card.where(q="set.id:base1") 
+
+    # Only original base 1-6 cards
+    cards = Card.where(q="set.series:Base")
+
+    
+    # Loop through every card to collect/store relevant information in json file
+    for card in cards:
+        card_data = {
+            "id": card.id,
+            "name": card.name,
+            "hp": getattr(card, "hp", None),
+            "types": getattr(card, "types", None),
+            "moves": [
+                {
+                    "name": move.name,
+                    "damage": move.damage,
+                    "text": move.text
+                }
+                for move in (card.attacks or [])
+            ],
+            "rarity": getattr(card, "rarity", None),
+            "set": card.set.name if card.set else None,
+            "image_url": card.images.large if card.images else None,
+        }
+
+        # Add current card data to the list
+        all_cards.append(card_data)
+
+        # Print validation message
+        print(f"Fetched: {card.name} ({card.id})")
+    
+    # Save all_cards list as json file
+    with open(output_path, "w") as f:
+        json.dump(all_cards, f, indent=2)
+    
+    # Print validation message
+    print(f"\nDone. {len(all_cards)} cards saved to {output_path}")
+
+# Run the function
+get_cards()
+
+"""
+STEPS TO RUN:
+
+python ./data_collection/data_collection.py
+
+"""
---
+++ data_collection/download_card_images.py
@@ -0,0 +1,58 @@
+import json
+import os
+import requests
+
+def download_images(metadata_path="backend/app/index/metadata.json", output_dir="./data_collection/images"):
+    #Mka ethe iamges folder
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Load the metadat.json file
+    with open(metadata_path, "r") as f:
+        cards = json.load(f)
+
+    # Loo through all cards in json file
+    for card in cards:
+        # Get the card url and id
+        image_url = card.get("image_url")
+        card_id = card.get("id")
+
+        # Skip the card if there is no image
+        if not image_url or not card_id:
+            continue
+
+        # Use card id as filename ex). "base1-4.jpg"
+        filename = os.path.join(output_dir, f"{card_id}.png")
+
+        # Skip the image if it is already in the images folder
+        if os.path.exists(filename):
+            print(f"Skipping {card_id} — already downloaded")
+            continue
+
+        # Try to get the image
+        try:
+            response = requests.get(image_url, timeout=10)
+
+            # Write the image to the images folder (f)
+            with open(filename, "wb") as f:
+                f.write(response.content)
+
+                # Print success message
+            print(f"Downloaded: {card_id}")
+
+        # Show error message
+        except Exception as e:
+            print(f"Failed: {card_id} — {e}")
+
+    # Print final message when done
+    print("\nFinished downloading images.")
+
+# Run the function
+download_images()
+
+"""
+STEPS TO RUN:
+
+python ./data_collection/download_card_images.py
+
+
+"""
